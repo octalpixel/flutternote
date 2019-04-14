588b9daf4ff04cf51345e5a9e707d641fd725091
mithushancj <mithushancj@gmail.com>
2019-04-14 13:47:39 +0530

    Integrated Full Creation Flow

--- lib/src/dao/note/note.dart
+++ lib/src/dao/note/note.dart
@@ -1,21 +1,52 @@
 import 'package:meta/meta.dart';
+import 'package:uuid/uuid.dart';
 
 class Note {
   // Note Properties
   String noteId;
   String noteTitle;
   String noteContent;
+  int createdAt;
+  int modifiedAt;
+
+  static var uuid = new Uuid();
 
   Note(
-      {@required this.noteId,
+      {this.noteId,
       @required this.noteTitle,
-      @required this.noteContent});
+      @required this.noteContent,
+      this.createdAt,
+      this.modifiedAt}) {
+    // Set up default values
+
+    if (noteId == null) {
+      noteId = uuid.v4();
+    }
+
+    if (createdAt == null) {
+      this.createdAt = DateTime.now().millisecondsSinceEpoch;
+    }
+
+    if (modifiedAt == null) {
+      this.modifiedAt = DateTime.now().millisecondsSinceEpoch;
+    }
+  }
 
   Map<String, dynamic> toMap() {
     return {
       'noteId': noteId,
       'noteTitle': noteTitle,
-      'noteContent': noteContent
+      'noteContent': noteContent,
+      'createdAt': this.createdAt
     };
+  }
+
+  static Note fromMap(Map<String, dynamic> map) {
+    return Note(
+        noteId: map['noteId'],
+        noteTitle: map['noteTitle'],
+        noteContent: map['content'],
+        createdAt: map['createdAt'],
+        modifiedAt: map['modifiedAt']);
   }
 }
--- lib/src/screens/notes/create_note_screen.dart
+++ lib/src/screens/notes/create_note_screen.dart
@@ -1,21 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutternote/src/dao/note/note.dart';
+import 'package:flutternote/src/dao/note/note_dao.dart';
 
-
-class CreateNoteScreen extends StatefulWidget{
- 
- _CreateNoteScreen createState() => _CreateNoteScreen();
-
+class CreateNoteScreen extends StatefulWidget {
+  _CreateNoteScreen createState() => _CreateNoteScreen();
 }
 
+class _CreateNoteScreen extends State<CreateNoteScreen> {
+  NoteDao noteDao = NoteDao();
 
-class _CreateNoteScreen extends State<CreateNoteScreen>{
+  int noteCount = 0 ;
 
-  
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+  void initState() {
+    
   }
 
-  
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController contentController = TextEditingController();
+
+    // Note tile
+    final noteTitle = TextField(
+      autofocus: true,
+      decoration: InputDecoration(hintText: 'Note Title'),
+      onChanged: (title) {
+        print('Note title Entered => $title');
+      },
+      controller: titleController,
+    );
+    final noteContent = TextField(
+      decoration: InputDecoration(hintText: 'Note Content'),
+      onChanged: (content) {
+        print('Note  Content Entered =>  $content');
+      },
+      controller: contentController,
+    );
+
+    addNote() async {
+      Note note = Note(
+          noteTitle: titleController.value.toString(),
+          noteContent: contentController.value.toString());
+      await noteDao.insert(note);
+    
+    List<Note> notes = await noteDao.getAll();
+    this.setState((){
+      this.noteCount =notes.length;
+    });
+      
+     
+    }
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 24),
+        child: Column(
+          children: <Widget>[
+            noteTitle,
+            noteContent,
+            new RaisedButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              child: Text('Add note'),
+              onPressed: () {
+                print('This was excuted on press');
+                addNote();
+              },
+            ),
+            Text('Numbers of notes :  $noteCount')
+          ],
+        ),
+      ),
+    );
+  }
 }
